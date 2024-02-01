559f14608828082ceddb907d633b5057fe5ba834
Aeonstrife <j.v.jeoffer@gmail.com>
2024-02-01 19:53:05 +0530

    Update config.h

--- keyboards/buildakb/m0110rp/config.h
+++ keyboards/buildakb/m0110rp/config.h
@@ -5,7 +5,7 @@
 
 #define DIODE_DIRECTION COL2ROW
 
-#define MATRIX_ROW_PINS { GP15, GP12, GP2, GP1, GP0 }
+#define MATRIX_ROW_PINS { GP0, GP1, GP2, GP12, GP15 }
 #define MATRIX_COL_PINS { GP11, GP14, GP16, GP17, GP18, GP19, GP20, GP21, GP22, GP8, GP26, GP27, GP13, GP28 }
 
 /* Mechanical locking support. Use KC_LCAP, KC_LNUM or KC_LSCR instead in keymap */
